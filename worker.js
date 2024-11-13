c6b7fc01f2df389b76db117d952d608e411bf570
Michael <mihasso@protonmail.com>
2024-11-13 22:38:12 +0100

    update worker with loki

--- worker.js
+++ worker.js
@@ -7,15 +7,38 @@ module.exports = {
         const country = headers.get('CF-IPCountry');
 
         const info = {
-            time: Date.now(),
-            tzTime: new Date(),
             ip,
             userAgent,
             referer,
             country,
         }
 
-        console.log(JSON.stringify(info, null, 2));
+        const log = JSON.stringify(info);
+        const logEntry = {
+            streams: [
+                {
+                    stream: { job: "hasso-visitors" },
+                    values: [
+                        [String(Date.now() * 1000000), log]
+                    ]
+                }
+            ]
+        };
+        const logData = JSON.stringify(logEntry);
+
+        const lokiUrl = `${env.LOKI_URL}/loki/api/v1/push`;
+        const username = env.LOKI_USERNAME;
+        const password = env.LOKI_PASSWORD;
+        const auth = "Basic " + btoa(`${username}:${password}`);
+
+        ctx.waitUntil(fetch(lokiUrl, {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": auth
+            },
+            body: logData
+        }));
 
         return new Response('OK', {
             status: 200,
