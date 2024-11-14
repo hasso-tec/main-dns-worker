7a122718777859512e6b9f090d228ff7b9725c1d
Michael <mihasso@protonmail.com>
2024-11-14 22:19:08 +0100

    provide manual redirect

--- worker.js
+++ worker.js
@@ -1,6 +1,7 @@
 export default {
     async fetch(request, env, ctx) {
         const {hostname} = new URL(request.url);
+        // Exceptions
         if (
             [
                 env.GRAFANA_URL,
@@ -10,49 +11,57 @@ export default {
             // skip
             return fetch(request);
         }
-
         const {headers} = request;
-        const ip = headers.get('CF-Connecting-IP');
-        const userAgent = headers.get('User-Agent');
-        const referer = headers.get('Referer') || "Direct";
-        const country = headers.get('CF-IPCountry');
+        logRequest(headers, hostname, env, ctx);
 
-        const info = {
-            ip,
-            userAgent,
-            referer,
-            hostname,
-            country,
+        // Redirect MAIN_URL to REDIRECT_URL
+        if (hostname === env.MAIN_URL) {
+            return Response.redirect(`https://${env.REDIRECT_URL}`, 301);
         }
-
-        const log = JSON.stringify(info);
-        const logEntry = {
-            streams: [
-                {
-                    stream: {job: "visitors"},
-                    values: [
-                        [String(Date.now() * 1000000), log]
-                    ]
-                }
-            ]
-        };
-        const logData = JSON.stringify(logEntry);
-
-        const lokiUrl = `https://${env.LOKI_URL}/loki/api/v1/push`;
-        const username = env.LOKI_USERNAME;
-        const password = env.LOKI_PASSWORD;
-        const auth = "Basic " + btoa(`${username}:${password}`);
-
-        ctx.waitUntil(fetch(lokiUrl, {
-            method: 'POST',
-            headers: {
-                "Content-Type": "application/json",
-                "Authorization": auth
-            },
-            body: logData
-        }));
 
         // continue
         return fetch(request);
     }
-};
+}
+
+function logRequest(headers, hostname, env, ctx) {
+    const ip = headers.get('CF-Connecting-IP');
+    const userAgent = headers.get('User-Agent');
+    const referer = headers.get('Referer') || "Direct";
+    const country = headers.get('CF-IPCountry');
+
+    const info = {
+        ip,
+        userAgent,
+        referer,
+        hostname,
+        country,
+    }
+
+    const log = JSON.stringify(info);
+    const logEntry = {
+        streams: [
+            {
+                stream: {job: "visitors"},
+                values: [
+                    [String(Date.now() * 1000000), log]
+                ]
+            }
+        ]
+    };
+    const logData = JSON.stringify(logEntry);
+
+    const lokiUrl = `https://${env.LOKI_URL}/loki/api/v1/push`;
+    const username = env.LOKI_USERNAME;
+    const password = env.LOKI_PASSWORD;
+    const auth = "Basic " + btoa(`${username}:${password}`);
+
+    ctx.waitUntil(fetch(lokiUrl, {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": auth
+        },
+        body: logData
+    }));
+}
